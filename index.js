54894aaf5780c1b6d316d96b369ea13974c6acd2
Anh Cao <caodunganh@gmail.com>
2015-09-04 23:43:04 +0700

    update code to get many song type

--- index.js
+++ index.js
@@ -4,44 +4,143 @@ const async = require('async');
 const fs = require('fs');
 const http = require('http');
 
-const baseUrl = 'http://hopamviet.com/chord/rhythm/1/bolero/';
+var baseUrls = [
+{
+    name: 'bolero',
+    url: 'http://hopamviet.com/chord/rhythm/1/bolero'
+}, {
+    name: 'slow',
+    url: 'http://hopamviet.com/chord/rhythm/2/slow'
+}, {
+    name: 'slow-rock',
+    url: 'http://hopamviet.com/chord/rhythm/3/slow-rock'
+}, {
+    name: 'slow-surf',
+    url: 'http://hopamviet.com/chord/rhythm/4/slow-surf'
+}, {
+    name: 'blues',
+    url: 'http://hopamviet.com/chord/rhythm/5/blues'
+}, {
+    name: 'ballade',
+    url: 'http://hopamviet.com/chord/rhythm/6/ballade'
+}, {
+    name: 'chachacha',
+    url: 'http://hopamviet.com/chord/rhythm/7/chachacha'
+}, {
+    name: 'disco',
+    url: 'http://hopamviet.com/chord/rhythm/8/disco'
+}, {
+    name: 'rhumba',
+    url: 'http://hopamviet.com/chord/rhythm/9/rhumba'
+}, {
+    name: 'tango',
+    url: 'http://hopamviet.com/chord/rhythm/10/tango'
+}, {
+    name: 'boston',
+    url: 'http://hopamviet.com/chord/rhythm/11/boston'
+}, {
+    name: 'fox',
+    url: 'http://hopamviet.com/chord/rhythm/12/fox'
+}, {
+    name: 'rock',
+    url: 'http://hopamviet.com/chord/rhythm/13/rock'
+}, {
+    name: 'valse',
+    url: 'http://hopamviet.com/chord/rhythm/14/valse'
+}, {
+    name: 'bossa-nova',
+    url: 'http://hopamviet.com/chord/rhythm/15/bossa-nova'
+}, {
+    name: 'pop',
+    url: 'http://hopamviet.com/chord/rhythm/16/pop'
+}, {
+    name: 'habanera',
+    url: 'http://hopamviet.com/chord/rhythm/17/habanera'
+}, {
+    name: 'twist',
+    url: 'http://hopamviet.com/chord/rhythm/18/twist'
+}, {
+    name: 'march',
+    url: 'http://hopamviet.com/chord/rhythm/19/march'
+}, {
+    name: 'pasodope',
+    url: 'http://hopamviet.com/chord/rhythm/20/pasodope'
+}, {
+    name: 'slow-ballad',
+    url: 'http://hopamviet.com/chord/rhythm/21/slow-ballad'
+}, {
+    name: 'rap',
+    url: 'http://hopamviet.com/chord/rhythm/22/rap'
+}]
 
-var urls = [];
-var curPage = 0;
-var isEnd = false;
+var totalCount = 0;
+
 
 function main() {
     //load through pages
-    async.doWhilst(
-        function (callback) {
-            getDownloadUrls(baseUrl, curPage, urls, function (err, isPageEnd) {
+    async.eachSeries(baseUrls, function (item, callback) {
+        var baseUrl = item.url;
+        var curPage = 0;
+        var urls = [];
+        var isEnd = false;
+
+        async.doWhilst(
+            function (callback) {
+                getDownloadUrls(baseUrl, curPage, urls, function (err, isPageEnd) {
+                    if (err) {
+                        return callback(err);
+                    };
+
+                    if (isPageEnd) {
+                        isEnd = isPageEnd;
+                        return callback();
+                    };
+
+                    console.log('Successfully get all songs from page: ' + (curPage + 1) + ' of type: ' + item.name);
+                    curPage++;
+                    callback();
+                });
+            }, function () {
+                return !isEnd;
+            }, function (err) {
                 if (err) {
                     return callback(err);
                 };
 
-                if (isPageEnd) {
-                    isEnd = isPageEnd;
-                    return callback();
-                };
+                var data = {
+                    urls: urls,
+                    count: urls.length,
+                    type: item.name
+                }
 
-                console.log('Successfully get songs from page ' + (curPage + 1));
-                curPage++;
-                callback();
+                var dataStr = '';
+                urls.forEach(function (item) {
+                    if (!item.url) {
+                        return;
+                    };
+
+                    dataStr += item.url + '\n';
+                })
+
+                totalCount += urls.length;
+
+                fs.writeFileSync(item.name + '-pretty.json', JSON.stringify(data, null, 4));
+                fs.writeFileSync(item.name + '.txt',dataStr);
+                console.log('Successfully get ' + urls.length + ' songs of type: ' + item.name);
+                return callback();
             });
-        }, function () {
-            return !isEnd;
-        }, function (err) {
-            if (err) {
-                console.trace();
-            };
+    }, function (err) {
+        if (err) {
+            console.log(err.stack);
+            console.trace();
+        };
 
-            fs.writeFileSync('out.json', JSON.stringify(urls, null, 4));
-            console.log('Successfull');
-        });
+        console.log('Successfull get ' + totalCount + ' songs');
+    })
 }
 
-//main();
-downloadSongs();
+main();
+//downloadSongs();
 
 function downloadSongs() {
     var urls = fs.readFileSync('out.json');
@@ -61,7 +160,8 @@ function downloadSongs() {
 }
 
 function getDownloadUrls(baseUrl, curPage, urls, callback) {
-    var curPageUrl = baseUrl + curPage * 10;
+    var curPageUrl = baseUrl + '/' + curPage * 10;
+    debugger;
 
     request.get(curPageUrl, function (err, res, html) {
         if (err) {
